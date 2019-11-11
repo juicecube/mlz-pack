34f6db08aef27f7be8a56bca2b2b9c8ac2d1e0ce
lichengfeng <lichengfeng@codemao.cn>
2019-11-11 14:47:31 +0800

    feat(ts): 添加升级TS-3.7.2 && babel-plugin语法支持

--- src/webpack/babel.ts
+++ src/webpack/babel.ts
@@ -37,6 +37,8 @@ export const getBabelConfig = () => {
       ],
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
     ],
   };
   return babelCfg;
